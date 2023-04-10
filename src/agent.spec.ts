53d14a6339260db0e2c7dc4df754ee9f164c96ce
route-2 <ruthurao@gmail.com>
2023-04-11 01:49:58 +0530

    fixed:4 tests

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -26,14 +26,14 @@ const TEST_DATA_1 = {
   metadata: "abcdefghi",
 };
 const TEST_DATA_2 = {
-  agentId: BigNumber.from("4444"),
+  agentId: "4444",
   owner: createAddress("0x44"),
-  chainIds: [BigNumber.from("222")],
+  chainIds: "222",
   metadata: "jklmnopqr",
 };
 
 const TEST_DEPLOYER_ADDRESS = createAddress("0x123");
-const TEST_FORTA_ADDRESS = createAddress("0x456");
+const TEST_DEPLOYER_ADDRESS2 = createAddress("0x456");
 
 describe("Nethermind Agent", () => {
   let handleTransaction: HandleTransaction;
@@ -111,6 +111,72 @@ describe("Nethermind Agent", () => {
       }),
     ]);
   })
+
+
+  it("returns findings from multiple deployers", async() => {
+    txEvent = new TestTransactionEvent()
+    .setFrom(TEST_DEPLOYER_ADDRESS)
+    .setTo(FORTA_CONTRACT_ADDRESS)
+    .addTraces({
+      function: "" || fortaProxy.getFunction("createAgent") || undefined,
+      to: FORTA_CONTRACT_ADDRESS,
+      from: TEST_DEPLOYER_ADDRESS,
+
+      arguments: [
+        TEST_DATA_1.agentId,
+        TEST_DEPLOYER_ADDRESS,
+        TEST_DATA_1.metadata,
+        [BigNumber.from(TEST_DATA_1.chainIds[0])],
+      ],
+    })
+    .addTraces({
+      function: "" || fortaProxy.getFunction("createAgent") || undefined,
+      to: FORTA_CONTRACT_ADDRESS,
+      from: TEST_DEPLOYER_ADDRESS2,
+
+      arguments: [
+        TEST_DATA_2.agentId,
+        TEST_DEPLOYER_ADDRESS2,
+        TEST_DATA_2.metadata,
+        [BigNumber.from(TEST_DATA_2.chainIds[0])],
+      ],
+    });
+
+    findings = await handleTransaction(txEvent);
+    expect(findings).toStrictEqual([
+      Finding.fromObject({
+        name: "New Nethermind Bot Created",
+        description: `New bot deployed by NM`,
+        alertId: "FORTA-1",
+        severity: FindingSeverity.Info,
+        type: FindingType.Info,
+        metadata: {
+          agentId: TEST_DATA_1.agentId,
+          owner: TEST_DEPLOYER_ADDRESS,
+          chainIds: TEST_DATA_1.chainIds[0],
+          metadata: TEST_DATA_1.metadata,
+        },
+      }),
+      Finding.fromObject({
+        name: "New Nethermind Bot Created",
+        description: `New bot deployed by NM`,
+        alertId: "FORTA-1",
+        severity: FindingSeverity.Info,
+        type: FindingType.Info,
+        metadata: {
+          agentId: TEST_DATA_2.agentId,
+          owner: TEST_DEPLOYER_ADDRESS2,
+          chainIds: TEST_DATA_2.chainIds[0],
+          metadata: TEST_DATA_2.metadata,
+        },
+      }),
+    ]);
+
+
+
+  })
+
+ 
 
   
 });
